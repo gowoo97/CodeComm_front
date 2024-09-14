37cfc3e140161d8948a7597e1a8fc4e41f3d021e
wooseok <gowoo97@gmail.com>
2024-09-14 14:44:40 +0900

    feat: add signup

--- code_comm_back/app.js
+++ code_comm_back/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const db = require("./config/mysql.js");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+const conn = db.init();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 
 app.set('port',process.env.PORT || 8081);
@@ -8,6 +14,20 @@ app.set('port',process.env.PORT || 8081);
 app.get('/', (req,res) => {
     res.send('Hello, Express')
 })
+
+app.post('/auth/signup', function(req, res){
+    const sql = "insert into MEMBER (email,pw,nickname) values(?,?,?)";
+    const body = req.body;
+
+    const params = [body.email,body.pw,body.nickname];
+    conn.query(sql, params ,function(err,result){
+        if(err) console.log("query is not excuted: "+ err);
+        else{
+            return res.sendStatus(200);
+        }
+    })
+})
+
 
 
 
