2e26f5bdb6b32d96c694aa4aa6683797d68ff394
wooseok <gowoo97@gmail.com>
2024-09-14 14:15:34 +0900

    feat: config mysql

--- code_comm_back/app.js
+++ code_comm_back/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require("./config/mysql.js");
 const app = express();
 
 
@@ -7,6 +8,8 @@ app.set('port',process.env.PORT || 8081);
 app.get('/', (req,res) => {
     res.send('Hello, Express')
 })
+
+
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중')
---
+++ code_comm_back/config/mysql.js
@@ -0,0 +1,21 @@
+var mysql = require("mysql2");
+
+var db_info = {
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password:"Gowoo97!",
+    database:"codeComm"
+}
+
+module.exports = {
+    init: function () {
+        return mysql.createConnection(db_info);
+    },
+    connect: function(conn){
+        conn.connect(function(err){
+            if (err) console.error("mysql connection error : "+ err);
+            else console.log("mysql is connected successfully!");
+        })
+    }
+}
