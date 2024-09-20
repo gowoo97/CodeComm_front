d797cd8ef39779b3ceae7c8c30754febf0ed3177
wooseok <gowoo97@gmail.com>
2024-09-20 15:45:02 +0900

    feat: 프론트에서 uuid 받기

--- code_comm/src/organisms/SignupInputs.tsx
+++ code_comm/src/organisms/SignupInputs.tsx
@@ -7,9 +7,12 @@ import { RootState } from "../app/store";
 import { setEmail, setCode,setPassword , setPasswordConfirm , setNickname } from "../slices/signupSlice";
 import { useDispatch, useSelector } from "react-redux";
 import axiosInstance from "../app/api";
-
+import { useState } from "react";
 
 const SignupInputs:React.FC = ()=> {
+
+    const [UUID,setUUID] = useState('');
+
     const dispatch = useDispatch();
     const { email,code,password, passwordConfirm, nickname} = useSelector((state:RootState) => state.signup);
 
@@ -22,10 +25,20 @@ const SignupInputs:React.FC = ()=> {
             })
     }
 
+    const getCode = async function(){
+        axiosInstance.post("/auth/verify", {
+            to:email
+        })
+        .then((res)=> {
+            setUUID(res.data.uuid);
+            console.log(res.data.uuid);
+        })
+    }
+
     return(
         <div className={styles.container}>
             <InputAndButton label="이메일 요청" placeholder="이메일" onChange={(e) => dispatch(setEmail(e.target.value))} 
-            onClick={()=>{}}></InputAndButton>
+            onClick={()=>{getCode()}}></InputAndButton>
             <InputAndButton label="인증" placeholder="인증번호" onChange={(e)=> dispatch(setCode(e.target.value))}
             onClick={()=>{}}></InputAndButton>
             <Input width="270px" height="31px" placeholder="비밀번호" onChange={(e)=> dispatch(setPassword(e.target.value))}></Input>
